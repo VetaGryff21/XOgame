3a5f1e17fe1a4fb3bc7e7ab02bf3a3ffdfef48c1
Veta Yurkina <vetagryff21@gmail.com>
2022-03-11 16:50:58 +0500

    Create Player

---
+++ src/main/java/com/vetagryff21/xogame/model/Player.java
@@ -0,0 +1,20 @@
+package com.vetagryff21.xogame.model;
+
+public class Player {
+
+    private final String name;
+    private final Figure figure;
+
+    public Player(String name, Figure figure) {
+        this.name = name;
+        this.figure = figure;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+}
